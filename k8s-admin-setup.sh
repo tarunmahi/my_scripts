8d48b3b11f1ff7e995c9210745ae2e4f5d5abc1f
tarun-Mahi <77262613+tarunmahi@users.noreply.github.com>
2025-03-13 15:21:59 +0530

    Create k8s-admin-setup.sh

---
+++ k8s-admin-setup.sh
@@ -0,0 +1,21 @@
+set -e 
+sudo apt-get update -y
+#install docker 
+echo "installing docker"
+sudo apt-get install docker.io -y
+sudo apt-get install git -y
+sudo systemctl start docker 
+sudo usermod -aG docker $USER
+
+echo "installing kubernetes"
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+
+echo "all the processess have been done"
+kubeadm version 
